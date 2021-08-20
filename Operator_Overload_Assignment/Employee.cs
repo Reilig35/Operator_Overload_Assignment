8d3f3593a9fcff5fa85814b1fdb21259dbf51a39
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-08-20 19:22:13 +0100

    Add project files.

---
+++ Operator_Overload_Assignment/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operator_Overload_Assignment
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            if (emp1.Id.Equals(emp2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            if (!emp1.Id.Equals(emp2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+
+
+
+
---
+++ Operator_Overload_Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Operator_Overload_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee william = new Employee();
+            william.Id = 1234;
+            william.Name = "William";
+            william.Department = ".Net Developer";
+
+            Employee susan = new Employee();
+            susan.Id = 1235;
+            susan.Name = "Susan";
+            susan.Department = ".Net Developer";
+
+            if (william == susan)
+            {
+                Console.WriteLine($"{william.Name}'s and {susan.Name}'s Id's are equal");
+            }
+            else if (william != susan)
+            {
+                Console.WriteLine($"{william.Name}'s and {susan.Name}'s Id's are not equal");
+            }
+            Console.Read();
+        }
+    }
+}
+
+
+        
+        
+           
+    
+
+       
+        
+
+
+        
+
+
